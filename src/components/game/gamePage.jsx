d60c126e8bf22c346697faf08be53fd56ca52dc5
phong1233 <phong1233@gmail.com>
2020-03-24 13:53:25 -0400

    fix spelling mistake

--- src/components/game/gamePage.jsx
+++ src/components/game/gamePage.jsx
@@ -13,6 +13,6 @@ function GamePage() {
   );
 }
 
-const snakeDescription = 'Eat as many red cubes as you can. Your snake will grow as you eat. Beweare going out of bound or eating yourself will result in death.';
+const snakeDescription = 'Eat as many red cubes as you can. Your snake will grow as you eat. Beware going out of bound or eating yourself will result in death.';
 
 export default GamePage;
